418d612238443c6a9e936a214755645acf1c7414
Da <taoda001@gmail.com>
2013-11-10 17:37:03 -0600

    Panel now associates with a model.

--- MagicTetris/src/org/MagicTetris/UIFragment/BoardPanel.java
+++ MagicTetris/src/org/MagicTetris/UIFragment/BoardPanel.java
@@ -1,6 +1,8 @@
 package org.MagicTetris.UIFragment;
 
 import javax.swing.JPanel;
+
+import org.MagicTetris.Models.BoardPanelModel;
 /**
  * UI fragment. Showing a player's board.
  * Graphic effect from item use will be show on this.
@@ -8,5 +10,6 @@ import javax.swing.JPanel;
  *
  */
 public class BoardPanel extends JPanel {
+	private BoardPanelModel model;
 
 }
