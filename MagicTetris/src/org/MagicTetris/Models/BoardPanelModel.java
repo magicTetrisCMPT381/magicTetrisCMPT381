34b674c26ba856aae29341a9eb2342eb8ecde5e8
Da <taoda001@gmail.com>
2013-11-10 17:37:45 -0600

    Implemented some feature of model.

--- MagicTetris/src/org/MagicTetris/Models/BoardPanelModel.java
+++ MagicTetris/src/org/MagicTetris/Models/BoardPanelModel.java
@@ -7,4 +7,52 @@ package org.MagicTetris.Models;
  */
 public class BoardPanelModel {
 
+	/**
+	 * The number of columns on the board.
+	 */
+	public final int COLUMN_COUNT = 12;
+	/**
+	 * The number of visible rows on the board.
+	 */
+	public final int VISIBLE_ROW_COUNT = 24;
+	/**
+	 * The number of hidden rows on the board.
+	 * For pre-draw blocks.
+	 */
+	public final int HIDDEN_ROW_COUNT = 2;
+	/**
+	 * Total rows on the board.
+	 */
+	public final int TOTAL_ROW_COUNT = VISIBLE_ROW_COUNT + HIDDEN_ROW_COUNT;
+	
+	private SingleBlock[][] board = new SingleBlock[TOTAL_ROW_COUNT][COLUMN_COUNT];
+	
+	/**
+	 * Represents the status of each block in the board.
+	 * @author Da
+	 *
+	 */
+	private class SingleBlock{
+		/**
+		 * Is the point occupied by any block.
+		 */
+		private boolean isOccupied;
+		/**
+		 * Is the point frozen( could not be cleared ).
+		 */
+		private boolean isFrozen;
+		
+		protected boolean isOccupied() {
+			return isOccupied;
+		}
+		protected void setOccupied(boolean isOccupied) {
+			this.isOccupied = isOccupied;
+		}
+		protected boolean isFrozen() {
+			return isFrozen;
+		}
+		protected void setFrozen(boolean isFrozen) {
+			this.isFrozen = isFrozen;
+		}
+	}
 }
