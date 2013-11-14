71d32a2ba27e47a4e970703be597340e2fe4f637
DeadSakura <taoda001@gmail.com>
2013-11-13 20:38:52 -0600

    Added PlayerController.

--- MagicTetris/src/org/MagicTetris/Models/PlayerController.java
+++ MagicTetris/src/org/MagicTetris/Models/PlayerController.java
@@ -1,11 +1,48 @@
 package org.MagicTetris.Models;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
- * A controller response to user input.
- * Receive the effect from item use.
- * @author Da
+ *  Key listener for receiving player action.
+ *  
  *
  */
-public class PlayerController {
+public class PlayerController extends KeyAdapter {
+	KeyEvent rotate;
+	KeyEvent down;
+	KeyEvent left;
+	KeyEvent right;
+	KeyEvent useItem;
+	KeyEvent changeItem;
 	
+	
+	@SuppressWarnings("unused")
+	private PlayerController() {
+		// Block default constructor.
+	}
+
+	public PlayerController(KeyEvent rotateKey,
+							KeyEvent downKey,
+							KeyEvent leftKey,
+							KeyEvent rightKey,
+							KeyEvent useItemKey,
+							KeyEvent changeItemKey){
+		rotate = rotateKey;
+		down = downKey;
+		left = leftKey;
+		right = rightKey;
+		useItem = useItemKey;
+		changeItem = changeItemKey;
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		super.keyTyped(arg0);
+	}
+
+
 
 }
