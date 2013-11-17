742bc7bd6cae02a1d36ba0985889ce700aa29aad
DeadSakura <taoda001@gmail.com>
2013-11-16 18:24:10 -0600

    BoardPanel can draw block now.

--- MagicTetris/src/org/MagicTetris/UIFragment/BoardPanel.java
+++ MagicTetris/src/org/MagicTetris/UIFragment/BoardPanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import org.MagicTetris.Models.BoardPanelModel;
+import org.MagicTetris.Models.BoardPanelModel.SingleBlock;
 /**
  * UI fragment. Showing a player's board.
  * Graphic effect from item use will be show on this.
@@ -53,13 +54,34 @@ public class BoardPanel extends JPanel {
 	}
 	/**
 	 * Draw a block on board.
+	 * WARNING: prototype method. 
+	 * The real method should draw block by reading SingleBlock block[][] = model.getBoard().
 	 */
-	protected void drawBlock(Graphics2D g) {
-		
+	protected void drawBlock(Graphics g) {
+//		SingleBlock block[][] = model.getBoard();
+//		if (block != null) {
+			for (int x = 0; x < model.COLUMN_COUNT; x++) {
+				for (int y = 0; y < model.VISIBLE_ROW_COUNT; y++) {
+//					if (block[x][y] != null) {
+//						
+//					}
+					g.setColor(Color.RED);
+					g.fillRect(x * BLOCK_SIZE, 
+							y * BLOCK_SIZE, 
+							BLOCK_SIZE, BLOCK_SIZE);
+					g.setColor(Color.RED.darker());
+					g.fillRect(x * BLOCK_SIZE + BLOCK_SHADOW, 
+							y * BLOCK_SIZE + BLOCK_SHADOW, 
+							BLOCK_SIZE - 2 * BLOCK_SHADOW, BLOCK_SIZE - 2 * BLOCK_SHADOW);
+				}
+			}
+//		}
 	}
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		g.translate(5, 5);
+		drawBlock(g);
 		drawGrid(g);
 		
 	}
