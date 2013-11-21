4a8d9ea6a5dca8fcd212db5717ed67856f7f179c
DeadSakura <taoda001@gmail.com>
2013-11-21 00:38:43 -0600

    Change player score now.

--- MagicTetris/src/org/MagicTetris/util/playerTimerTask.java
+++ MagicTetris/src/org/MagicTetris/util/playerTimerTask.java
@@ -4,7 +4,9 @@ import java.util.TimerTask;
 
 import org.MagicTetris.Models.BoardPanelModel;
 import org.MagicTetris.Models.Player;
+import org.MagicTetris.Models.StatusPanelModel;
 import org.MagicTetris.UIFragment.BoardPanel;
+import org.MagicTetris.UIFragment.StatusPanel;
 
 /**
  * This class is to control the dropping speed of blocks.
@@ -13,21 +15,30 @@ import org.MagicTetris.UIFragment.BoardPanel;
  *
  */
 public class playerTimerTask extends TimerTask{
-	private BoardPanelModel model;
-	private BoardPanel panel;
+	private BoardPanelModel bpm;
+	private BoardPanel bp;
+	private StatusPanelModel spm;
+	private StatusPanel sp;
+	private Player player;
 
 	
-	public playerTimerTask(BoardPanel p, BoardPanelModel m) {
-		panel = p;
-		model = m;
+	public playerTimerTask(Player p) {
+		player = p;
+		bp = p.getBoardPanel();
+		sp = p.getStatusPanel();
+		bpm = p.getBoardPanelModel();
+		spm = p.getStatusPanelModel();
 	}
 
 	@Override
 	public void run() {
-		model.moveCurrentPieceDown();
-		if (!model.isGameOver()) {
-			model.clearLines();
-			panel.repaint();
+		bpm.moveCurrentPieceDown();
+		if (!bpm.isGameOver()) {
+			int clearedLine = bpm.clearLines();
+			int currentScore = spm.getScore();
+			spm.setScore(currentScore + 100 * clearedLine);
+			sp.update();
+			bp.repaint();
 		}
 
 		
