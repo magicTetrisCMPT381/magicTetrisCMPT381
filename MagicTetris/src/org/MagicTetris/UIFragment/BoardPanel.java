75409a7070fcaf594e8d9d5e6140a251fe966863
Da <taoda001@gmail.com>
2013-11-22 10:54:56 -0600

    Reduced block size due to main window size reduction.

--- MagicTetris/src/org/MagicTetris/UIFragment/BoardPanel.java
+++ MagicTetris/src/org/MagicTetris/UIFragment/BoardPanel.java
@@ -21,11 +21,11 @@ public class BoardPanel extends JPanel {
 	/**
 	 * The size of a block.
 	 */
-	public static final int BLOCK_BASE = 24;
+	public static final int BLOCK_BASE = 17;
 	/**
 	 * The shadow width of a block
 	 */
-	public static final int BLOCK_SHADOW = 4;
+	public static final int BLOCK_SHADOW = 3;
 	
 	public static final int BLOCK_SIZE = BLOCK_SHADOW + BLOCK_BASE;
 	
