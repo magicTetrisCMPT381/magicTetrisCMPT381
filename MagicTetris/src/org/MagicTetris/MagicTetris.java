fc724a0cc452e32118893c2c6c1c59cb941d3d38
DeadSakura <taoda001@gmail.com>
2013-11-24 14:57:50 -0600

    Added internal class for control game start.

--- MagicTetris/src/org/MagicTetris/MagicTetris.java
+++ MagicTetris/src/org/MagicTetris/MagicTetris.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 
@@ -20,8 +22,8 @@ import org.MagicTetris.UIFragment.BoardPanel;
 @SuppressWarnings("serial")
 public class MagicTetris extends JFrame {
 
-	Player player1;
-	Player player2;
+	private Player player1;
+	private Player player2;
 	
 	public MagicTetris() {
 		super("Magic Tetris");
@@ -30,23 +32,19 @@ public class MagicTetris extends JFrame {
 		setLayout(new GridBagLayout());
 		
 		createPlayer();
-		
-		add(player1.getBoardPanel());
-		add(player1.getStatusPanel());
-		add(player2.getBoardPanel());
-		add(player2.getStatusPanel());
+		addPanels();
+		addDefaultControls();
+
 		setLayout(new GridLayout(1,0));
 		requestFocus();
 		
-		addKeyListener(player1.getPlayerController());
-		addKeyListener(player2.getPlayerController());
 		pack();
 		setResizable(false);
 		setVisible(true);
 
 	}
 	
-	public void createPlayer() {
+	protected void createPlayer() {
 		player1 = new Player();
 		player2 = new Player();
 		Integer[][] piece1 = player1.getBoardPanelModel().createNextPiece();
@@ -66,6 +64,19 @@ public class MagicTetris extends JFrame {
 		player2.getPlayerController().setDefaultControlKeys(2);
 	}
 	
+	protected void addPanels(){
+		add(player1.getBoardPanel());
+		add(player1.getStatusPanel());
+		add(player2.getBoardPanel());
+		add(player2.getStatusPanel());
+	}
+	
+	protected void addDefaultControls() {
+		addKeyListener(player1.getPlayerController());
+		addKeyListener(player2.getPlayerController());
+		addKeyListener(new mainFrameController(this));
+	}
+	
 	public void startGame() {
 		player1.startGame();
 		player2.startGame();
@@ -78,21 +89,28 @@ public class MagicTetris extends JFrame {
 	 */
 	public static void main(String[] args) {
 		MagicTetris frame = new MagicTetris();
-		frame.startGame();
-		
-		
-		
+//		frame.startGame();
+	}
+	
+	private class mainFrameController extends KeyAdapter{
+		private final int DEFAULT_START_KEY = KeyEvent.VK_ENTER;
+		private final int OPTION_KEY = KeyEvent.VK_ESCAPE;
+		private MagicTetris frame;
 
+		public mainFrameController(MagicTetris frame) {
+			this.frame = frame;
+		}
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == DEFAULT_START_KEY) {
+				frame.startGame();
+				return;
+			}
+			if (e.getKeyCode() == OPTION_KEY) {
+				
+			}
+		}
 		
-
-		
-
-		
-		
-
-		
-
-
 	}
 
 }
