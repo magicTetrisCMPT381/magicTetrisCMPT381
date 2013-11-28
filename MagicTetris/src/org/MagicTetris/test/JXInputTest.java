0d64b62265867a30148a12b88203801cbbae7b65
guoxingze <guoxingze@gmail.com>
2013-11-28 01:13:27 -0600

    delete jxinput

--- MagicTetris/src/org/MagicTetris/test/JXInputTest.java
+++ MagicTetris/src/org/MagicTetris/test/JXInputTest.java
@@ -1,24 +1,24 @@
-package org.MagicTetris.test;
-
-import de.hardcode.jxinput.JXInputManager;
-import de.hardcode.jxinput.directinput.DirectInputDevice;
-import de.hardcode.jxinput.event.JXInputEventManager;
-
-public class JXInputTest{
-
-        
-        
-        public static void main(String[] args){
-                
-                System.load("E:\\gitHub\\magicTetrisCMPT381\\MagicTetris\\jxinput.dll");
-                JXInputEventManager.setTriggerIntervall( 50 );
-                for(int i = 0; i < JXInputManager.getNumberOfDevices(); i++){
-                        if(JXInputManager.getJXInputDevice(i).getName().equals("Controller (XBOX 360 For Windows)")){
-                                DirectInputDevice xbox = new DirectInputDevice(i);
-                                System.out.println("This is XBOX");
-                      }
-                }
-
-        }
-
-}
+//package org.MagicTetris.test;
+//
+//import de.hardcode.jxinput.JXInputManager;
+//import de.hardcode.jxinput.directinput.DirectInputDevice;
+//import de.hardcode.jxinput.event.JXInputEventManager;
+//
+//public class JXInputTest{
+//
+//        
+//        
+//        public static void main(String[] args){
+//                
+//                System.load("E:\\gitHub\\magicTetrisCMPT381\\MagicTetris\\jxinput.dll");
+//                JXInputEventManager.setTriggerIntervall( 50 );
+//                for(int i = 0; i < JXInputManager.getNumberOfDevices(); i++){
+//                        if(JXInputManager.getJXInputDevice(i).getName().equals("Controller (XBOX 360 For Windows)")){
+//                                DirectInputDevice xbox = new DirectInputDevice(i);
+//                                System.out.println("This is XBOX");
+//                      }
+//                }
+//
+//        }
+//
+//}
