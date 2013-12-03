b13ed159d5d31923657cfec8c2265e9cbf1ba782
DeadSakura <taoda001@gmail.com>
2013-12-03 02:18:35 -0600

    Method for test added

--- MagicTetris/src/org/MagicTetris/MagicTetris.java
+++ MagicTetris/src/org/MagicTetris/MagicTetris.java
@@ -13,7 +13,11 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.plaf.LayerUI;
 
+import org.MagicTetris.GameItems.MagicAtom;
+import org.MagicTetris.GameItems.MagicCleaner;
 import org.MagicTetris.GameItems.MagicFlash;
+import org.MagicTetris.GameItems.MagicFreezer;
+import org.MagicTetris.GameItems.MagicShield;
 import org.MagicTetris.Models.Player;
 import org.MagicTetris.UIFragment.EffectLayer;
 import org.MagicTetris.UIFragment.OptionPanel;
@@ -110,7 +114,16 @@ public class MagicTetris extends JFrame {
 	}
 	
 	private void Tester() {
+		playerOne.getStatusPanelModel().addItem(new MagicAtom());
 		playerOne.getStatusPanelModel().addItem(new MagicFlash());
+		playerOne.getStatusPanelModel().addItem(new MagicFreezer());
+		playerOne.getStatusPanelModel().addItem(new MagicShield());
+		playerOne.getStatusPanelModel().addItem(new MagicCleaner());
+		playerTwo.getStatusPanelModel().addItem(new MagicAtom());
+		playerTwo.getStatusPanelModel().addItem(new MagicFlash());
+		playerTwo.getStatusPanelModel().addItem(new MagicFreezer());
+		playerTwo.getStatusPanelModel().addItem(new MagicShield());
+		playerTwo.getStatusPanelModel().addItem(new MagicCleaner());
 	}
 
 	/**
